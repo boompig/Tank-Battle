b9fd13bc5e248661cd6ad23ac987b1142b6038fe
Daniel Kats <boompigdev@gmail.com>
2013-08-01 13:43:57 -0400

    auto-formatting

--- application/views/account/loginForm.php
+++ application/views/account/loginForm.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 
 <html>
@@ -9,33 +8,31 @@
 			}
 		</style>
 
-	</head> 
-<body>  
-	<h1>Login</h1>
-<?php 
-	if (isset($errorMsg)) {
-		echo "<p>" . $errorMsg . "</p>";
-	}
+	</head>
+	<body>
+		<h1>Login</h1>
+		<?php
+			if (isset($errorMsg)) {
+				echo "<p>" . $errorMsg . "</p>";
+			}
 
+			echo form_open('account/login');
+			echo form_label('Username');
+			echo form_error('username');
+			echo form_input('username', set_value('username'), "required");
+			echo form_label('Password');
+			echo form_error('password');
+			echo form_password('password', '', "required");
 
-	echo form_open('account/login');
-	echo form_label('Username'); 
-	echo form_error('username');
-	echo form_input('username',set_value('username'),"required");
-	echo form_label('Password'); 
-	echo form_error('password');
-	echo form_password('password','',"required");
-	
-	echo form_submit('submit', 'Login');
-	
-	echo "<p>" . anchor('account/newForm','Create Account') . "</p>";
+			echo form_submit('submit', 'Login');
 
-	echo "<p>" . anchor('account/recoverPasswordForm','Recover Password') . "</p>";
-	
-	
-	echo form_close();
-?>	
-</body>
+			echo "<p>" . anchor('account/newForm', 'Create Account') . "</p>";
+
+			echo "<p>" . anchor('account/recoverPasswordForm', 'Recover Password') . "</p>";
+
+			echo form_close();
+		?>
+	</body>
 
 </html>
 
