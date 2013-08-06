a620d6ba06d7a5aa0e2b7e415181712a8d22353a
Daniel Kats <boompigdev@gmail.com>
2013-08-06 16:27:58 -0400

    Formattted newForm view

--- application/views/account/newForm.php
+++ application/views/account/newForm.php
@@ -1,56 +1,76 @@
-
 <!DOCTYPE html>
 
 <html>
 	<head>
+		<title>Create Account | CSC309 Assignment 3</title>
+		<meta charset="UTF-8" />
+
 		<style>
 			input {
 				display: block;
 			}
 		</style>
+		
+		<!-- Google-hosted JQuery -->
 		<script src="http://code.jquery.com/jquery-latest.js"></script>
+		
 		<script>
+			$(function() {
+				//console.log('');
+				//console.log('');
+			});
+		
+			/**
+			 * HTML5 password checking
+			 */
 			function checkPassword() {
-				var p1 = $("#pass1"); 
+				var p1 = $("#pass1");
 				var p2 = $("#pass2");
-				
+
 				if (p1.val() == p2.val()) {
-					p1.get(0).setCustomValidity("");  // All is well, clear error message
+					p1.get(0).setCustomValidity("");
+					// All is well, clear error message
 					return true;
-				}	
-				else	 {
+				} else {
 					p1.get(0).setCustomValidity("Passwords do not match");
 					return false;
 				}
 			}
 		</script>
-	</head> 
-<body>  
-	<h1>New Account</h1>
-<?php 
-	echo form_open('account/createNew');
-	echo form_label('Username'); 
-	echo form_error('username');
-	echo form_input('username',set_value('username'),"required");
-	echo form_label('Password'); 
-	echo form_error('password');
-	echo form_password('password','',"id='pass1' required");
-	echo form_label('Password Confirmation'); 
-	echo form_error('passconf');
-	echo form_password('passconf','',"id='pass2' required oninput='checkPassword();'");
-	echo form_label('First');
-	echo form_error('first');
-	echo form_input('first',set_value('first'),"required");
-	echo form_label('Last');
-	echo form_error('last');
-	echo form_input('last',set_value('last'),"required");
-	echo form_label('Email');
-	echo form_error('email');
-	echo form_input('email',set_value('email'),"required");
-	echo form_submit('submit', 'Register');
-	echo form_close();
-?>	
-</body>
+	</head>
+	<body>
+		<h1>New Account</h1>
+		<?php
+			echo form_open('account/createNew');
+			
+			echo form_label('Username');
+			echo form_error('username');
+			echo form_input('username', set_value('username'), "required");
+			
+			echo form_label('Password');
+			echo form_error('password');
+			echo form_password('password', '', "id='pass1' required");
+			
+			echo form_label('Password Confirmation');
+			echo form_error('passconf');
+			echo form_password('passconf', '', "id='pass2' required oninput='checkPassword();'");
+			
+			echo form_label('First');
+			echo form_error('first');
+			echo form_input('first', set_value('first'), "required");
+			
+			echo form_label('Last');
+			echo form_error('last');
+			echo form_input('last', set_value('last'), "required");
+			
+			echo form_label('Email');
+			echo form_error('email');
+			echo form_input('email', set_value('email'), "required");
+			
+			echo form_submit('submit', 'Register');
+			echo form_close();
+		?>
+	</body>
 
 </html>
 
